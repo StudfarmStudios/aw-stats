68e3af110d40f611ea76ae60b8817ff1894ee0b9
Jaakko Lukkari <jaakko.lukkari@gmail.com>
2011-12-03 12:34:27 +0200

    Added posibility to get userdata by username

--- http.js
+++ http.js
@@ -6,8 +6,8 @@ var app = express.createServer();
 // TODO SOME KIND OF TOKEN FOR ALL THE ROUTES
 app.get('/pilot/create', middlewares.pilot.create(true));
 app.get('/pilot/list', middlewares.pilot.list(true));
-app.get('/pilot/:id', middlewares.pilot.loader(true));
-
+app.get('/pilot/id/:id', middlewares.pilot.loader(true));
+app.get('/pilot/:username', middlewares.pilot.loader(true));
 
 app.get('/login', middlewares.pilot.login(true));
 app.listen(3001, function (err) {
