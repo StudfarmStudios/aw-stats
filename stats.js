f67996c2651ded6ffce3f8d5ce1d47a719d82542
Jaakko Lukkari <jaakko.lukkari@gmail.com>
2011-12-07 19:24:21 +0200

    stats

--- stats.js
+++ stats.js
@@ -10,6 +10,34 @@ server.allow('82.181.67.118');
 // LOG EVERY REQUEST
 server.register({}, statsMiddlewares.log());
 
+server.register(
+    {Arena: {$exists:true}},
+    statsMiddlewares.arena()
+);
+
+server.register(
+    {Killer: {$exists:true}},
+    statsMiddlewares.parallel([
+      statsMiddlewares.getPilot('Killer', '_killer'),
+      statsMiddlewares.getPilot('Victim', '_victim')
+    ]),
+    statsMiddlewares.kill()
+);
+
+server.register(
+    {Suicide: {$exists:true}},
+    statsMiddlewares.parallel([
+      statsMiddlewares.getPilot('Suicide', '_victim')
+    ]),
+    statsMiddlewares.suicide()
+);
+
+server.register(
+    {$has : ['Ship', 'Weapon2', 'Device', 'Player']},
+    statsMiddlewares.getPilot('Player', '_pilot'),
+    statsMiddlewares.shipInfo()
+);
+
 // START LISTENING
 server.listen(function (err) {
   if (err) {
