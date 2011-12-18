7ad3ba32e42bc229a592c695a35f93ebc23851a2
Jaakko Lukkari <jaakko.lukkari@gmail.com>
2011-12-18 23:27:50 +0200

    Added ircbot

--- app.js
+++ app.js
@@ -6,9 +6,19 @@ if (numberOfWorkers > 4) {
 }
 
 if (cluster.isMaster) {
+
+  var irc = require('./irc');
+
   var workers = 0;
   for(var i = 0; i < numberOfWorkers; i++) {
-    cluster.fork();
+    var worker = cluster.fork();
+    worker.on('message', function (data) {
+      if (typeof data == "object") {
+        if (data.cmd && data.cmd == 'irc') {
+          irc.say(data.msg);
+        }
+      }
+    });
     workers++;
   }
 
@@ -19,7 +29,6 @@ if (cluster.isMaster) {
       cluster.fork();
     }, 1);
   });
-
 
 } else {
   console.log("Worker started " + process.pid);
---
+++ irc.js
@@ -0,0 +1,22 @@
+var irc = require('irc');
+var settings = require('./lib/settings');
+
+var connected = false;
+var messageBuffer = [];
+
+var client = new irc.Client(settings.irc.host, settings.irc.nick, { channels : [settings.irc.channel] });
+
+client.on('join', function () {
+  connected = true;
+  messageBuffer.forEach(function (msg) {
+    client.say(settings.irc.channel, msg);
+  });
+});
+
+exports.say = function (msg) {
+  if (connected) {
+    client.say(settings.irc.channel, msg);
+  } else {
+    messageBuffer.push(msg);
+  }
+};
