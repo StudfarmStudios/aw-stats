565d834853a1328dc8e05164e227a3b415ea2060
Jaakko Lukkari <jaakko.lukkari@gmail.com>
2011-12-18 23:31:35 +0200

    Added connecting message

--- irc.js
+++ irc.js
@@ -6,6 +6,10 @@ var messageBuffer = [];
 
 var client = new irc.Client(settings.irc.host, settings.irc.nick, { channels : [settings.irc.channel] });
 
+client.on('connect', function () {
+  console.log("IRC connected to " + settings.irc.host);
+});
+
 client.on('join', function () {
   connected = true;
   messageBuffer.forEach(function (msg) {
