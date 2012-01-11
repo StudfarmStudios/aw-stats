c5a245005f180ae0acff9a8c676ab9ebd4fe3dfa
Jaakko Lukkari <jaakko.lukkari@gmail.com>
2012-01-11 22:44:46 +0200

    Added ranking top1

--- http.js
+++ http.js
@@ -13,6 +13,8 @@ function defineRoutesAndMiddleware(app) {
   app.use(express.static(__dirname + '/site'));
 // TODO SOME KIND OF TOKEN FOR ALL THE ROUTES
 
+  app.get('/ranking/top1', middlewares.ranking.top1(true));
+
   app.get('/server/list', middlewares.server.list(true));
 
   app.get('/pilot/create', middlewares.pilot.create(true));
