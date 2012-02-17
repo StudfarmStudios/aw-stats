e08706387786b8c432bf96ec502faa1fedfee2b6
Jaakko Lukkari <jaakko.lukkari@gmail.com>
2012-02-17 20:02:40 +0200

    Use www.assaultwing.com

--- site/js/communication.js
+++ site/js/communication.js
@@ -1,5 +1,5 @@
 (function (window, undefined) {
-  var socket = io.connect();
+  var socket = io.connect("http://assaultwing.com:3003");
   socket.on('broadcast', function (data) {
     if (window.currentView && window.currentView.processBroadcast) {
       window.currentView.processBroadcast(data);
