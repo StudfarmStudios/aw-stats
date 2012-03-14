8c2c8ebe3196536d9c5c036b7aa75955dedea710
Jaakko Lukkari <jaakko.lukkari@gmail.com>
2012-03-14 23:19:26 +0200

    Fixed some bugs

--- masterserver.js
+++ masterserver.js
@@ -5,6 +5,7 @@ var server = require('./lib/masterserver').createMasterServer({port: 3003});
 server.register('addserver', middlewares.add());
 server.register('joinserver', middlewares.join());
 server.register('removeserver', middlewares.remove());
+server.register('updateserver', middlewares.update());
 server.register('listservers', middlewares.list(true));
 
 server.listen();
--- test/testmasterserver.js
+++ test/testmasterserver.js
@@ -1,8 +1,10 @@
 var dgram = require('dgram');
 
 var client = dgram.createSocket("udp4");
-var message = new Buffer("operation=joinserver;serverid=123");
-client.send(message, 0, message.length, 3003, "127.0.0.1");
+
+var serverprovider = require('../lib/repositories/server');
+
+
 client.on('message', function (msg) {
   var line = msg.toString();
   console.log(line);
@@ -13,6 +15,53 @@ client.on('message', function (msg) {
     var kv = part.split('=');
     server[kv[0]] = kv[1];
   });
-  client.close();
   console.log(server);
 });
+
+console.log("add")
+var message2 = new Buffer("operation=addserver;name=testi;maxclients=255;localendpoint=2313123321:231321:123321;awversion=31321213;tcpport=1132");
+client.send(message2, 0, message2.length, 3003, "127.0.0.1");
+
+setTimeout(function () {
+  console.log("list")
+  var message = new Buffer("operation=listservers");
+  client.send(message, 0, message.length, 3003, "127.0.0.1");
+  serverprovider.getServers(function (err, servers) {
+    var server = servers[0];
+    console.log("join")
+    var message = new Buffer("operation=joinserver;serverid=" + server.id);
+    client.send(message, 0, message.length, 3003, "127.0.0.1");
+
+    setTimeout(function () {
+      //operation=updateserver;currentclients=<current client #>
+      console.log("Update");
+      var message = new Buffer("operation=updateserver;currentclients=1");
+      client.send(message, 0, message.length, 3003, "127.0.0.1");
+
+      setTimeout(function () {
+         console.log("List");
+        var message = new Buffer("operation=listservers");
+        client.send(message, 0, message.length, 3003, "127.0.0.1");
+
+        setTimeout(function () {
+           console.log("Remove");
+          var message = new Buffer("operation=removeserver");
+          client.send(message, 0, message.length, 3003, "127.0.0.1");
+
+          setTimeout(function () {
+           console.log("List");
+           var message = new Buffer("operation=listservers");
+          client.send(message, 0, message.length, 3003, "127.0.0.1");
+
+
+        }, 1000);
+
+        }, 1000);
+
+      }, 1000);
+
+    }, 1000);
+
+  });
+
+}, 1000);
